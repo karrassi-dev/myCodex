3be27f544255a8f3418033bd712c2ccf149e95d5
karrassi-dev <karrassi.hamza@gmail.com>
2022-12-28 00:49:15 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_API_AI,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -17,10 +17,9 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello from CodeX !'
+        message: 'Hello from CodeX!'
     })
 })
-
 
 app.post('/', async (req, res) => {
     try {
